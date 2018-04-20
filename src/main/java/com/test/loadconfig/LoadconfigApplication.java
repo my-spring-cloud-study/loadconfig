722a0d72ac66f4f6fad9392598e8fc404ae8b8d5
lm93102 <louis.liu@yamibuy.com>
2018-04-20 09:34:30 +0800

    'init'

---
+++ src/main/java/com/test/loadconfig/LoadconfigApplication.java
@@ -0,0 +1,12 @@
+package com.test.loadconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadconfigApplication.class, args);
+	}
+}
